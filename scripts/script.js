8de59c9acb990343d50eea6303dde27a5c5e4ad5
KaitlinKCheng <80509426+KaitlinKCheng@users.noreply.github.com>
2021-06-14 21:09:22 -0500

    Add constants for shapes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+// Possible shapes to play
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
 /**
  * Generates a random shape for the computer to play.
  *
@@ -7,10 +12,10 @@
     let randomShape = Math.floor(Math.random() * 3) + 1;
 
     if (randomShape === 1) {
-        return 'rock';
+        return ROCK;
     } else if (randomShape === 2) {
-        return 'paper';
+        return PAPER;
     } else {
-        return 'scissors';
+        return SCISSORS;
     }
 }
