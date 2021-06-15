9efa5429e2ef715fefa4c17fc27829bb245aecb1
KaitlinKCheng <80509426+KaitlinKCheng@users.noreply.github.com>
2021-06-14 23:34:28 -0500

    Add playRound and capitalize functions

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,18 @@ const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
 
+// Possible results
+const WIN = 'win';
+const LOSE = 'lose';
+const TIE = 'tie';
+
+
 /**
  * Generates a random shape for the computer to play.
  *
- * @return {string}    The shape the computer plays.
+ * @returns {string} The shape the computer plays.
  */
- function computerPlay() {
+function computerPlay() {
     let randomShape = Math.floor(Math.random() * 3) + 1;
 
     if (randomShape === 1) {
@@ -18,4 +24,56 @@ const SCISSORS = 'scissors';
     } else {
         return SCISSORS;
     }
+}
+
+/**
+ * Plays one round of rock paper scissors. Checks the player and computer
+ * selections to determine a winner.
+ *
+ * @param {string} playerSelection - The shape the player chooses to play.
+ * @param {string} computerSelection - The shape the computer chooses to play.
+ * @returns {string} A message describing the winner of this round.
+ */
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    // Ignore letter case
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        result = TIE;
+    } else {
+        result = LOSE; // Assume loss
+
+        // Check for win
+        if ((playerSelection === ROCK)
+                && (computerSelection === SCISSORS)) {
+            result = WIN;
+        } else if ((playerSelection === PAPER)
+                && (computerSelection === ROCK)) {
+            result = WIN;
+        } else if ((playerSelection === SCISSORS)
+                && (computerSelection === PAPER)) {
+            result = WIN;
+        }
+    }
+
+    // Generate result message
+    if (result === WIN) {
+        return `You win! ${capitalize(playerSelection)} beats ${computerSelection}.`;
+    } else if (result === LOSE) {
+        return `You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`;
+    } else {
+        return `It's a tie! You and the computer both chose ${playerSelection}.`;
+    }
+}
+
+/**
+ * Capitalizes the first letter of a string.
+ *
+ * @param {string} string - The string to capitalize.
+ * @returns {string} The capitalized string.
+ */
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
 }
