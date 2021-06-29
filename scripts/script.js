ba8f6e9a2504b9d226187c01c19538e2ef34aa2a
KaitlinKCheng <80509426+KaitlinKCheng@users.noreply.github.com>
2021-06-29 02:11:50 -0500

    Rewrite & restructure code to work with the UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// ***** Global Variables *****
+
 // Possible shapes to play
 const ROCK = 'rock';
 const PAPER = 'paper';
@@ -8,6 +10,75 @@ const WIN = 'win';
 const LOSE = 'lose';
 const TIE = 'tie';
 
+// Scores
+let computerScore = 0;
+let playerScore = 0;
+
+const playTo = 5; // The score to play to
+
+// Page elements
+const computerShape = document.getElementById('catbot-shape');
+const playerShape = document.getElementById('player-shape');
+const resultText = document.getElementById('result-text');
+const scoresText = document.getElementById('scores-text');
+const shapeBtns = document.querySelectorAll('.shape-btn');
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorsBtn = document.getElementById('scissors-btn');
+
+
+// ***** Setup *****
+setup();
+
+shapeBtns.forEach((btn) => {
+    btn.addEventListener('click', playRound);
+});
+
+
+// ***** Functions *****
+
+/**
+ * Sets up the game by setting initial text.
+ */
+function setup() {
+    resultText.textContent = 'Start by choosing one of the shapes!';
+    scoresText.textContent = `Your score: ${playerScore} vs CatBot's score: ${computerScore}`;
+}
+
+/**
+ * Plays one round of rock paper scissors. Determines the player's selection,
+ * then compares it to the computer's to check for a winner. Updates the results
+ * accordingly.
+ *
+ * @param {Event} e - The event that occured.
+ */
+function playRound(e) {
+    let playerSelection = playerPlay(e.target);
+    let computerSelection = computerPlay();
+    let result;
+
+    // Immediate tie if shapes are the same
+    if (playerSelection === computerSelection) {
+        result = TIE;
+    } else {
+        // Check for win
+        if ((playerSelection === ROCK && computerSelection === SCISSORS)
+                || (playerSelection === PAPER && computerSelection === ROCK)
+                || (playerSelection === SCISSORS && computerSelection === PAPER)) {
+            result = WIN;
+        } else {
+            result = LOSE;
+        }
+    }
+
+    updateChoices(result, playerSelection, computerSelection);
+    updateResult(result);
+
+    // End the game when someone reaches the max score
+    if (playerScore === playTo || computerScore === playTo) {
+        endGame();
+    }
+}
 
 /**
  * Generates a random shape for the computer to play.
@@ -15,112 +86,103 @@ const TIE = 'tie';
  * @returns {string} The shape the computer plays.
  */
 function computerPlay() {
-    let randomShape = Math.floor(Math.random() * 3) + 1;
+    const shapes = [ROCK, PAPER, SCISSORS];
+    return shapes[Math.floor(Math.random() * 3)];
+}
 
-    if (randomShape === 1) {
-        return ROCK;
-    } else if (randomShape === 2) {
-        return PAPER;
-    } else {
-        return SCISSORS;
+/**
+ * Determines the player's choice of shape based on the button they chose.
+ *
+ * @param {Object} btn - The button that was chosen.
+ * @returns {string} The shape the player plays.
+ */
+function playerPlay(btn) {
+    switch (btn) {
+        case rockBtn:
+            return ROCK;
+        case paperBtn:
+            return PAPER;
+        case scissorsBtn:
+            return SCISSORS;
     }
 }
 
 /**
- * Plays one round of rock paper scissors. Checks the player and computer
- * selections to determine a winner.
+ * Updates everyone's shape text & styles based on the result & chosen shapes.
  *
- * @param {string} playerSelection - The shape the player chooses to play.
- * @param {string} computerSelection - The shape the computer chooses to play.
- * @returns {string} The constant WIN, LOSE, or TIE that reflects the player's result.
+ * @param {string} result - The result of the round.
+ * @param {string} playerSelection - The shape the player chose.
+ * @param {string} computerSelection - The shape the computer chose.
  */
-function playRound(playerSelection, computerSelection) {
-    let result;
+function updateChoices(result, playerSelection, computerSelection) {
+    // Clear styles from previous round
+    playerShape.classList.remove('win', 'lose', 'tie');
+    computerShape.classList.remove('win', 'lose', 'tie');
 
-    // Ignore letter case
-    playerSelection = playerSelection.toLowerCase();
+    // Change the text to match the chosen shape
+    playerShape.textContent = playerSelection;
+    computerShape.textContent = computerSelection;
 
-    if (playerSelection === computerSelection) {
-        result = TIE;
-    } else {
-        result = LOSE; // Assume loss
-
-        // Check for win
-        if ((playerSelection === ROCK)
-                && (computerSelection === SCISSORS)) {
-            result = WIN;
-        } else if ((playerSelection === PAPER)
-                && (computerSelection === ROCK)) {
-            result = WIN;
-        } else if ((playerSelection === SCISSORS)
-                && (computerSelection === PAPER)) {
-            result = WIN;
-        }
-    }
-
-    return result;
-}
-
-/**
- * Capitalizes the first letter of a string.
- *
- * @param {string} string - The string to capitalize.
- * @returns {string} The capitalized string.
- */
-function capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1);
-}
-
-/**
- * Plays a game of rock paper scissors using prompts and the console. Plays a
- * predetermined number of rounds, printing messages in the console for the
- * player.
- */
-function game() {
-    const numRounds = 5; // Number of rounds to play
-    let playerScore = 0;
-    let computerScore = 0;
-
-    console.log(`Time to play rock, paper, scissors! Number of rounds to play is set to: ${numRounds}.`);
-
-    for (let i = 0; i < numRounds; i++) {
-        let playerSelection = prompt('Pick a shape: [rock], [paper], or [scissors].');
-
-        if (playerSelection === null) {
-            console.log('Stopping the game...');
+    // Add styles based on results
+    switch (result) {
+        case WIN:
+            playerShape.classList.add('win');
+            computerShape.classList.add('lose');
             break;
-        } else {
-            let computerSelection = computerPlay();
-            let result = playRound(playerSelection, computerSelection);
-
-            // Generate result message
-            if (result === WIN) {
-                console.log(`You win! ${capitalize(playerSelection)} beats ${computerSelection}.`);
-                playerScore++;
-            } else if (result === LOSE) {
-                console.log(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);
-                computerScore++;
-            } else {
-                console.log(`It's a tie! You and the computer both chose ${playerSelection}.`);
-            }
-
-            console.log(`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
-        }
+        case LOSE:
+            playerShape.classList.add('lose');
+            computerShape.classList.add('win');
+            break;
+        case TIE:
+            playerShape.classList.add('tie');
+            computerShape.classList.add('tie');
+            break;
     }
-
-    if (playerScore > computerScore) {
-        console.log('Congratulations, you won!');
-    } else if (playerScore < computerScore) {
-        console.log('Oh no, you lost!');
-    } else {
-        console.log('You tied with the computer! You\'re both winners this time!');
-    }
-
-    console.log('Final scores:');
-    console.log(`Your score: ${playerScore}. Computer's score: ${computerScore}.`);
 }
 
+/**
+ * Uses the result to update the page's text and player & computer scores.
+ *
+ * @param {string} result - The result of the round.
+ */
+function updateResult(result) {
+    switch (result) {
+        case WIN:
+            resultText.textContent = 'You win this round! CatBot beeps in defeat.';
+            playerScore++;
+            break;
+        case LOSE:
+            resultText.textContent = 'You lose this round! CatBot meows triumphantly.';
+            computerScore++;
+            break;
+        case TIE:
+            resultText.textContent = 'It\'s a tie! CatBot intensifies its focus.';
+            break;
+    }
 
-// Start game
-const startGame = document.getElementById('start-game');
-startGame.addEventListener('click', game);
+    scoresText.textContent = `Your score: ${playerScore} vs CatBot's score: ${computerScore}`;
+}
+
+/**
+ * Sets the game to inactive, then updates the page's text to display the
+ * results of the game.
+ */
+function endGame() {
+    if (playerScore === playTo || computerScore === playTo) {
+        // Set game buttons to an inactive state
+        shapeBtns.forEach((btn) => {
+            btn.removeEventListener('click', playRound);
+            btn.classList.add('game-over');
+        });
+
+        if (playerScore === playTo) {
+            resultText.textContent = 'Game over! You beat CatBot.'
+                    + ' Even though it lost, CatBot enjoyed the game very much.';
+        } else {
+            resultText.textContent = 'Game over! CatBot won this time.'
+                    + ' It does a victory dance and thanks you for playing.';
+        }
+
+        resultText.textContent += ' Refresh the page to play again!';
+    }
+}
